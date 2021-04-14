99744447937d639175c22bcea75f7f70cf737a21
iDVB <me@danvanbrunt.com>
2021-04-14 16:31:44 -0400

    WIP

--- dist/es/Hero/Hero.js
+++ dist/es/Hero/Hero.js
@@ -3,48 +3,21 @@ import React from 'react';
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import styled from 'styled-components';
-import { reduceImages } from '../_common/utils.js';
 
 var _templateObject, _templateObject2, _templateObject3, _templateObject4;
-var sizeLookup = {
-  small: {
-    xs: 200,
-    md: 250,
-    lg: 300
-  },
-  medium: {
-    xs: 300,
-    md: 400,
-    lg: 500
-  },
-  large: {
-    xs: 400,
-    md: 500,
-    lg: 600
-  }
-};
-var MAIN_NAV_OFFSET = {
-  mobile: '56px',
-  desktop: '74px'
-};
 
 var Hero = function Hero(props) {
-  var imageQuery = props.imageQuery,
-      _props$imageFilename = props.imageFilename,
-      imageFilename = _props$imageFilename === void 0 ? 'image-filename' : _props$imageFilename,
-      _props$objectPosition = props.objectPosition,
+  props.imageQuery;
+      props.imageFilename;
+      var _props$objectPosition = props.objectPosition,
       objectPosition = _props$objectPosition === void 0 ? '50% 100%' : _props$objectPosition,
       _props$heading = props.heading,
       heading = _props$heading === void 0 ? 'Put Heading Here' : _props$heading,
-      overlay = props.overlay,
-      _props$size = props.size,
-      size = _props$size === void 0 ? 'medium' : _props$size,
-      minHeight = props.minHeight;
+      overlay = props.overlay;
+      props.size;
+      props.minHeight;
       _objectWithoutProperties(props, ["imageQuery", "imageFilename", "objectPosition", "heading", "overlay", "size", "minHeight"]);
 
-  var imagesLandscape = reduceImages(imageQuery.landscape.edges);
-  var imgSources = imagesLandscape[imageFilename];
-  var actualMinHeight = minHeight !== null && minHeight !== void 0 ? minHeight : sizeLookup[size];
   return /*#__PURE__*/React.createElement("div", null, overlay && /*#__PURE__*/React.createElement(Overlay, null), /*#__PURE__*/React.createElement(ImgHolder, null, /*#__PURE__*/React.createElement(StyledImg, {
     image: imgSources,
     objectFit: "cover",
@@ -55,11 +28,7 @@ var Hero = function Hero(props) {
   }, /*#__PURE__*/React.createElement(Box, {
     position: "relative",
     zIndex: "3",
-    paddingTop: {
-      xs: MAIN_NAV_OFFSET.mobile,
-      md: MAIN_NAV_OFFSET.desktop
-    },
-    minHeight: actualMinHeight,
+    minHeight: 100,
     display: "flex",
     alignItems: "center",
     justifyContent: "center"
@@ -68,15 +37,9 @@ var Hero = function Hero(props) {
   }, heading))));
 };
 
-var StyledHeading = styled.h1(_templateObject || (_templateObject = _taggedTemplateLiteral(["\n  position: relative;\n  margin: 0;\n  color: ", ";\n  z-index: 2;\n"])), function (_ref) {
-  var theme = _ref.theme;
-  return theme.palette.text.primary;
-});
+var StyledHeading = styled.h1(_templateObject || (_templateObject = _taggedTemplateLiteral(["\n  position: relative;\n  margin: 0;\n  color: black;\n  z-index: 2;\n"])));
 var ImgHolder = styled.div(_templateObject2 || (_templateObject2 = _taggedTemplateLiteral(["\n  position: absolute;\n  width: 100%;\n  height: 100%;\n  z-index: 2;\n"])));
-var Overlay = styled.div(_templateObject3 || (_templateObject3 = _taggedTemplateLiteral(["\n  top: 0;\n  position: absolute;\n  height: 100%;\n  width: 100%;\n  background: ", ";\n  z-index: 3;\n  opacity: 0.3;\n"])), function (_ref2) {
-  var theme = _ref2.theme;
-  return theme.palette.background["default"];
-});
+var Overlay = styled.div(_templateObject3 || (_templateObject3 = _taggedTemplateLiteral(["\n  top: 0;\n  position: absolute;\n  height: 100%;\n  width: 100%;\n  background: white;\n  z-index: 3;\n  opacity: 0.3;\n"])));
 var StyledImg = styled.div(_templateObject4 || (_templateObject4 = _taggedTemplateLiteral(["\n  height: 100%;\n"])));
 
 export default Hero;
--- src/Hero/Hero.js
+++ src/Hero/Hero.js
@@ -1,33 +1,7 @@
 import React from 'react'
 import Box from '@material-ui/core/Box'
 import Container from '@material-ui/core/Container'
-// import { GatsbyImage } from 'gatsby-plugin-image'
 import styled from 'styled-components'
-
-import { reduceImages } from '../_common/utils'
-
-const sizeLookup = {
-  small: {
-    xs: 200,
-    md: 250,
-    lg: 300,
-  },
-  medium: {
-    xs: 300,
-    md: 400,
-    lg: 500,
-  },
-  large: {
-    xs: 400,
-    md: 500,
-    lg: 600,
-  },
-}
-
-const MAIN_NAV_OFFSET = {
-  mobile: '56px',
-  desktop: '74px',
-}
 
 const Hero = (props) => {
   const {
@@ -40,11 +14,6 @@ const Hero = (props) => {
     minHeight,
     ...rest
   } = props
-
-  const imagesLandscape = reduceImages(imageQuery.landscape.edges)
-  const imgSources = imagesLandscape[imageFilename]
-
-  const actualMinHeight = minHeight ?? sizeLookup[size]
 
   return (
     <div>
@@ -61,11 +30,7 @@ const Hero = (props) => {
         <Box
           position="relative"
           zIndex="3"
-          paddingTop={{
-            xs: MAIN_NAV_OFFSET.mobile,
-            md: MAIN_NAV_OFFSET.desktop,
-          }}
-          minHeight={actualMinHeight}
+          minHeight={100}
           display="flex"
           alignItems="center"
           justifyContent="center"
@@ -80,7 +45,7 @@ const Hero = (props) => {
 const StyledHeading = styled.h1`
   position: relative;
   margin: 0;
-  color: ${({ theme }) => theme.palette.text.primary};
+  color: black;
   z-index: 2;
 `
 
@@ -96,7 +61,7 @@ const Overlay = styled.div`
   position: absolute;
   height: 100%;
   width: 100%;
-  background: ${({ theme }) => theme.palette.background.default};
+  background: white;
   z-index: 3;
   opacity: 0.3;
 `
